947120ac722ccd0c3888776a49a115f02d6a0871
Marley Alford <marleycalford@gmail.com>
2021-05-08 15:43:43 -0400

    Exercise 3.5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const e = require("express");
 const { request, response } = require("express");
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 let persons = [
   {
@@ -30,6 +30,10 @@ const info = `<p>Phonebook has info for ${
   persons.length
 } people</p><p>${new Date()}</p>`;
 
+const generateId = () => {
+  return Math.floor(Math.random() * 100000);
+};
+
 app.get("/", (request, response) => {
   response.send("<h1>Phonebook!</h1>");
 });
@@ -56,6 +60,22 @@ app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   persons = persons.filter((p) => p.id != id);
   response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!body.name || !body.number) {
+    return response
+      .status(400)
+      .json({ error: "Please provide both name and number" });
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+  persons = persons.concat(person);
+  response.json(person);
 });
 
 const PORT = 3001;
