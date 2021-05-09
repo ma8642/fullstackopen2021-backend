bf51aeac31d769b7841fdea02bd8f26ba6d6a9e7
Marley Alford <marleycalford@gmail.com>
2021-05-09 01:31:33 -0400

    Exercise 3.11 - uibuild

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors());
+app.use(express.static("build"));
 const morgan = require("morgan");
 app.use(express.json());
 morgan.token("body", function (req, res) {
