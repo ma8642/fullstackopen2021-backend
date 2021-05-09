7d11840ca5417fda705d6f0d6005709adb696e69
Marley Alford <marleycalford@gmail.com>
2021-05-09 14:18:02 -0400

    Exercise 3.13

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const { request, response } = require("express");
 const express = require("express");
 const app = express();
@@ -17,6 +18,8 @@ morgan.token("body", function (req, res) {
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
+
+const Person = require("./models/person");
 
 let persons = [
   {
@@ -57,7 +60,9 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
@@ -96,7 +101,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ models/person.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const url = process.env.MONGODB_URI;
+console.log("connecting to", url);
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Person", personSchema);
